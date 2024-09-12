4ea68289e59ed3251ba2c1116f112f112c7ca0af
DanielMagero <mdmagero700@gmail.com>
2024-09-12 17:11:37 +0300

    js start
    my first walk in js

---
+++ first_js_program.js
@@ -0,0 +1,7 @@
+// This is my first Javascript application
+console.log("Hello World");
+
+// Python equivalent is print()
+console.log("Mugulo Daniel Magero");
+console.log("M24B38/017");
+console.log("BSDS");
---
+++ variables.js
@@ -0,0 +1,43 @@
+//variables
+//Python 
+//age = 78
+//area = 78.4
+
+
+//Java 
+//int age = 78
+//float area = 78.4
+
+//Three methods of this is Js
+
+//#1 var
+
+//Initilization
+var my_name = "Daniel";
+
+//Declare a variable
+var height;
+//height = 0
+
+//assign a variable
+height = 3.4;
+
+console.log(my_name);
+console.log(height);
+
+
+//#2 let
+let area = 90.5;
+
+let f_name;
+
+f_name = "Daniel";
+
+//comst
+const pie = 3.14
+
+//can't declare a constant, it must be initialized
+//const  k;
+
+//can't assign under const
+//k = 34;
