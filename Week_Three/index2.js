4be7e32a4148ef70d9ca1b57f8447d358d078f79
DanielMagero <mdmagero700@gmail.com>
2024-09-18 15:07:45 +0300

    revision in js

---
+++ Week_Three/index2.js
@@ -0,0 +1,19 @@
+console.log("Hello World!");
+
+let first_name = "Jane";
+let last_name = "Doe";
+
+//advisable to write constants in uppercase
+const pie = 3.14;
+const MAX_SPEED = 4000;
+
+var is_student = false;
+
+var school_name = "UCU";
+
+//one line comments
+/*
+Multi
+line 
+comments
+*/
