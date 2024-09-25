4f4759b6e9bcaf4c56ab50a83ba7d9bfe2ab4a60
DanielMagero <mdmagero700@gmail.com>
2024-09-25 16:06:48 +0300

    js_loops

---
+++ Week_4/loops.js
@@ -0,0 +1,37 @@
+/*
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+
+//Exercise 1
+for (let number = 1; number <= 10; number++) {
+  console.log(`The number is ${number}`);
+}
+
+for (let num = 1; num <= 12; num++) {
+  console.log(num, "X 5 = ", num * 5);
+  console.log(`${num} X 5 = ${num * 5}`);
+}
+
+""
+''
+``
+
+let i = 1;
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+*/
+
+let answer = 0;
+while (answer !== "yes") {
+  answer = prompt("Do you want to continue? (yes/no)");
+}
