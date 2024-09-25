67ab830ed519a03a38dacb3f7e46c7362b8ba2ed
DanielMagero <mdmagero700@gmail.com>
2024-09-25 11:38:48 +0300

    Update techqueens.js

--- Week_Three/techqueens.js
+++ Week_Three/techqueens.js
@@ -7,13 +7,13 @@ console.log(completedChallenges);
 
 //Challenge 2: Amina's Amazing Functions
 function celebrateWin() {
-  console.log("You did it, Queen!");
+    console.log("You did it, Queen!");
 }
 
 celebrateWin;
 
 function celebrateWin(challengeName) {
-  console.log(`Challenge: ${challengeName} completed.`);
+    console.log(`Challenge: ${challengeName} completed.`);
 }
 
 var challengeName = 5;
@@ -21,40 +21,40 @@ celebrateWin(challengeName);
 
 //Challenge 3: Lina's Legendary Loops
 for (let i = 1; i <= 3; i++) {
-  console.log(`Challenge ${i} solved!`);
+    console.log(`Challenge ${i} solved!`);
 }
 
 for (let i = 1; i <= 3; i++) {
-  console.log(`The Tech Queens have solved Challenge ${i}!`);
+    console.log(`The Tech Queens have solved Challenge ${i}!`);
 }
 
 //Challenge 4: The Conditional Path of Wisdom
 var nextChallenge = "loops";
 if (nextChallenge === "loops") {
-  console.log("Time to practice loops!");
+    console.log("Time to practice loops!");
 } else {
-  console.log("On to the next challenge!");
+    console.log("On to the next challenge!");
 }
 
 nextChallenge = "functons";
 
 if (nextChallenge === "loops") {
-  console.log("Time to practice loops!");
+    console.log("Time to practice loops!");
 }
 if (nextChallenge === "functions") {
-  console.log("Time to master functions!");
+    console.log("Time to master functions!");
 } else {
-  console.log("On to the next challenge!");
+    console.log("On to the next challenge!");
 }
 
 //Bonus Challenge: The Queen's Secret Challenge
 
 function revealMessage(string) {
-  if (string.length > 0) {
-    return string[0];
-  } else {
-    return null;
-  }
+    if (string.length > 0) {
+        return string[0];
+    } else {
+        return null;
+    }
 }
 
 console.log(revealMessage("Queens"));
