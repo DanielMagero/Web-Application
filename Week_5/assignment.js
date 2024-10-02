5baacf404afb9162346269cd757a4ee0f60fa3d5
DanielMagero <mdmagero700@gmail.com>
2024-10-02 15:48:36 +0300

    functions_and_loops_assignment

---
+++ Week_5/assignment.js
@@ -0,0 +1,25 @@
+//Wtite a function called computation//It should add, subtract, divide, and multiply
+
+function arithmetic_function(num1, num2) {
+  var sum = num1 + num2;
+  var difference = num1 - num2;
+  var quotient = num1 / num2;
+  var product = num1 * num2;
+
+  console.log(
+    "The two numbers are",
+    num1,
+    "and ",
+    num2,
+    ". \nThe sum of the two numbers is",
+    sum,
+    "\nThe difference of the two numbers is: ",
+    difference,
+    "\nThe quotient of the two numbers is: ",
+    quotient,
+    "\nThe product of the two numbers is: ",
+    product
+  );
+}
+
+arithmetic_function(12, 32);
