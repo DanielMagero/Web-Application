0d80295dafae23043ffebec466bd253d0f5f0d79
DanielMagero <mdmagero700@gmail.com>
2024-10-10 18:01:50 +0300

    revision2

---
+++ Week_6/revision2.js
@@ -0,0 +1,10 @@
+//Question 2
+var rainfall_amount;
+
+//This is because the rainfall amount will change everyday
+
+var studentName = "Daniel";
+const totalIT = 38;
+console.log(
+  `The total number of students in the IT class is ${totalIT} and one of the students is ${studentName}`
+);
