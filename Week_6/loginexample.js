8e6b9ffb9ffd7da4161026de4bf033998b548efe
DanielMagero <mdmagero700@gmail.com>
2024-10-10 18:02:58 +0300

    examples

---
+++ Week_6/loginexample.js
@@ -0,0 +1,10 @@
+var email = "kingo@gmail.com";
+var phone = "2560000000000";
+var password = "1234";
+
+if (
+  (email == "kingo@gmail.com" || phone == "2560000000000") &&
+  password == "1234"
+) {
+  console.log("Welcome!");
+}
---
+++ Week_6/testvar.js
@@ -0,0 +1,5 @@
+var king = 1;
+let queen = 6;
+
+var king = 3;
+//let queen = 6
