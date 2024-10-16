6d56a1357d02450edfed3c8faa9de2692dec9d3a
DanielMagero <mdmagero700@gmail.com>
2024-10-16 15:48:04 +0300

    loops

---
+++ Week_7/RevisionLoops.js
@@ -0,0 +1,20 @@
+//FOR LOOP
+//for(initialize iterator; condition; increment)
+for (let x = 1; x <= 6; x++) {
+  console.log("x - ", x);
+}
+
+for (let x = 1; x <= 6; x++) {
+  if (x % 2 == 0) {
+    console.log("x - ", x);
+  }
+}
+
+//WHILELOOP
+//while(condition){execution}
+
+let j = 0;
+while (j < 6) {
+  console.log("x -", j);
+  j++;
+}
---
+++ Week_7/Revision_LoopsQns.js
@@ -0,0 +1,8 @@
+//An infinite loop is one that will run until the computer's memory runs out
+
+//Manipulating conditions
+//If one doesn't sets a condition like as long as the iterator is
+//Place a condition that it is always true
+//Not placing a condition
+
+//You would press the Ctrl + c or loop for the interrupt button
