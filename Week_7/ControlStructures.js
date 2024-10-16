f47378dc9eb192fc011382326f8931b421773d5b
DanielMagero <mdmagero700@gmail.com>
2024-10-16 15:48:24 +0300

    switch_statements

---
+++ Week_7/ControlStructures.js
@@ -0,0 +1,17 @@
+//IF STATEMMENTS
+
+//SWITCH STATEMENTS
+let score = 80;
+switch (score) {
+  case 100:
+    console.log("A Grade");
+  //break;
+  case 90:
+    console.log("A grade");
+  //break;
+  case 76:
+    console.log("B grade");
+  //break;
+  default:
+    console.log("Fail");
+}
