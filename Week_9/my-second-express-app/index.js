e122892bb99e2823b34f70fb249f44dc469844be
DanielMagero <mdmagero700@gmail.com>
2024-10-30 16:06:04 +0300

    update

--- Week_9/my-second-express-app/index.js
+++ Week_9/my-second-express-app/index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+//GET
+
+app.get("/greetings", (request, response) => {
+  response.send({ msg: "Hello Daniel!" });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
