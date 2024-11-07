8cba45c34b175f52d9ddd35656154317ad2377ff
DanielMagero <mdmagero700@gmail.com>
2024-11-07 18:19:40 +0300

    changes to db

--- Week_9/my-second-express-app/models/my_db.js
+++ Week_9/my-second-express-app/models/my_db.js
@@ -1,8 +1,16 @@
-//List of friends
-let friends = [
-  { id: 1, name: "Tracy" },
-  { id: 2, name: "Humble" },
-  { id: 3, name: "Esther" },
-  { id: 4, name: "Justin" },
-  { id: 5, name: "Anitah" },
+//list of students
+
+let students = [
+  { id: 1, AccessNo: "B24536", StudentName: "Tracy" },
+  { id: 2, AccessNo: "B24536", StudentName: "Anitah" },
+  { id: 3, AccessNo: "B28490", StudentName: "Joy" },
+  { id: 4, AccessNo: "B24536", StudentName: "Esther" },
+  { id: 5, AccessNo: "B24536", StudentName: "Justin" },
+  { id: 6, AccessNo: "B24536", StudentName: "Cynthia" },
+  { id: 7, AccessNo: "B24536", StudentName: "Elijah" },
+  { id: 8, AccessNo: "B24536", StudentName: "Humble" },
+  { id: 9, AccessNo: "B24536", StudentName: "Emmanuel" },
+  { id: 10, AccessNo: "B24536", StudentName: "Cathy" },
 ];
+
+module.exports = students;
