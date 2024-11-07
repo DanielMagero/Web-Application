21ceecff9513b55f39cd2a847e6a63fb73f80827
DanielMagero <mdmagero700@gmail.com>
2024-11-07 16:20:54 +0300

    creating a model

--- Week_9/my-second-express-app/index.js
+++ Week_9/my-second-express-app/index.js
@@ -8,14 +8,6 @@ app.get("/greetings", (request, response) => {
   return response.send({ msg: "Hello Daniel!" });
 });
 
-//List of friends
-let friends = [
-  { id: 1, name: "Tracy" },
-  { id: 2, name: "Humble" },
-  { id: 3, name: "Esther" },
-  { id: 4, name: "Justin" },
-  { id: 5, name: "Anitah" },
-];
 app.get("/list-of-friends", (request, response) => {
   return response.send(friends);
 });
---
+++ Week_9/my-second-express-app/models/my_db.js
@@ -0,0 +1,8 @@
+//List of friends
+let friends = [
+  { id: 1, name: "Tracy" },
+  { id: 2, name: "Humble" },
+  { id: 3, name: "Esther" },
+  { id: 4, name: "Justin" },
+  { id: 5, name: "Anitah" },
+];
